966859f303313d130b6f70028e85df2ef1722a88
Douglas F <douglastelec@gmail.com>
2024-03-26 12:07:08 -0300

    dasa

---
+++ dist/index.mjs
@@ -0,0 +1,1 @@
+import o from"express";import{createServer as e}from"http";var n=o(),s=e(n),r=8081;s.listen(r,()=>{console.log(`Server is running on port ${r}`)});
---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import express, {Request, Response} from 'express';
+import {createServer} from 'http';
+
+const app = express();
+
+app.get("/", (req: Request, res: Response)=>{
+    res.send("Hello World!");
+})
+
+app.get("/healthcheck", (req: Request, res: Response)=>{
+    try{
+        res.status(200).send("OK");
+    } catch (error){
+        res.status(500).send();
+    }
+})
+
+app.get("/api/v1/todos", (req: Request, res: Response)=>{
+    res.send("GET TODOS");
+});
+
+app.get("/api/v1/todos/:id", (req: Request, res: Response)=>{
+    res.send("GET TODOS BY ID");
+});
+
+app.post("/api/v1/todos/:id", (req: Request, res: Response)=>{
+    res.send("POST CREATE TODO");
+});
+
+app.patch("/api/v1/todos/:id", (req: Request, res: Response)=>{
+    res.send("UPDATE TODO BY ID");
+});
+
+app.delete("/api/v1/todos/:id", (req: Request, res: Response)=>{
+    res.send("DELETE TODO BY ID");
+});
+
+const server = createServer(app);
+
+
+const port = 8081;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+});
